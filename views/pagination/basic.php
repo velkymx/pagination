078f286df1efbb7290fba6c5aa36e97d79e8d102
velkymx <velkymx@gmail.com>
2011-06-05 10:36:00 -0700

    Edited views/pagination/basic.php via GitHub

--- views/pagination/basic.php
+++ views/pagination/basic.php
@@ -1,20 +1,20 @@
 <p class="pagination">
 
-	<?php if ($first_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($first_page)) ?>" rel="first"><?php echo __('First') ?></a>
+	<?php if ($_first_page !== FALSE): ?>
+		<a href="<?php echo HTML::chars($page->url($_first_page)) ?>" rel="first"><?php echo __('First') ?></a>
 	<?php else: ?>
 		<?php echo __('First') ?>
 	<?php endif ?>
 
-	<?php if ($previous_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($previous_page)) ?>" rel="prev"><?php echo __('Previous') ?></a>
+	<?php if ($_previous_page !== FALSE): ?>
+		<a href="<?php echo HTML::chars($page->url($_previous_page)) ?>" rel="prev"><?php echo __('Previous') ?></a>
 	<?php else: ?>
 		<?php echo __('Previous') ?>
 	<?php endif ?>
 
-	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
+	<?php for ($i = 1; $i <= $_total_pages; $i++): ?>
 
-		<?php if ($i == $current_page): ?>
+		<?php if ($i == $_current_page): ?>
 			<strong><?php echo $i ?></strong>
 		<?php else: ?>
 			<a href="<?php echo HTML::chars($page->url($i)) ?>"><?php echo $i ?></a>
@@ -22,14 +22,14 @@
 
 	<?php endfor ?>
 
-	<?php if ($next_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($next_page)) ?>" rel="next"><?php echo __('Next') ?></a>
+	<?php if ($_next_page !== FALSE): ?>
+		<a href="<?php echo HTML::chars($page->url($_next_page)) ?>" rel="next"><?php echo __('Next') ?></a>
 	<?php else: ?>
 		<?php echo __('Next') ?>
 	<?php endif ?>
 
-	<?php if ($last_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($last_page)) ?>" rel="last"><?php echo __('Last') ?></a>
+	<?php if ($_last_page !== FALSE): ?>
+		<a href="<?php echo HTML::chars($page->url($_last_page)) ?>" rel="last"><?php echo __('Last') ?></a>
 	<?php else: ?>
 		<?php echo __('Last') ?>
 	<?php endif ?>
